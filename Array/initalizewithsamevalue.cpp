6ef9ac3660d92bb856d0d6c7badeab615bd8de11
Anushka Sharma <anushkasharma156cs@gmail.com>
2026-05-11 09:25:02 +0000

    initial value code array

---
+++ Array/initalizewithsamevalue.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int arr[7];
+    cout<<"enter the no.-";
+    cin>>n;
+    for(int i=0;i<7;i++){
+        arr[i]=n;
+    cout<<arr[i]<<" ";
+    }
+
+   
+}
